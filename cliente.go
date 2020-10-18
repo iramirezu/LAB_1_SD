df91c51bb2c66fc9d4823a39b49fe1ae1a0d2f5c
iramirezu <ignacio@lemonpot.com>
2020-10-18 18:56:09 -0300

    pa

--- cliente.go
+++ cliente.go
@@ -1,6 +1,9 @@
 package main;
 
 import (
+	"encoding/csv"
+	"os"
+	
 	"log"
 	"fmt"
 	"golang.org/x/net/context"
--- pruebaExcel.go
+++ pruebaExcel.go
@@ -2,8 +2,8 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"log"
 	"os"
+	"log"
 )
 func main() {
 	rows := readSample()
