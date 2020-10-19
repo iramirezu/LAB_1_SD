7554d9a93aee8533f358945759b40b16de7d10c8
iramirezu <ignacio@lemonpot.com>
2020-10-19 05:01:21 -0300

    pa

--- Cliente/chatCliente/chatCliente.go
+++ Cliente/chatCliente/chatCliente.go
@@ -3,6 +3,7 @@ package chatCliente
 import (
 	"log"
 	"strconv"
+	"os"
 	"golang.org/x/net/context"
 )
 
@@ -11,26 +12,26 @@ type Server struct {
 
 func (s *Server) GenerarOrden(ctx context.Context, mensaje *OrdenGenerada) (*IdSeguimiento, error) {
 	IteradorIdSeguimiento := 1
-	id := mensaje.id
-	producto := mensaje.producto
-	valor := mensaje.valor
-	tienda := mensaje.tienda
-	destino := mensaje.destino
-	prioritario := mensaje.prioritario
+	id := mensaje.Id
+	producto := mensaje.Producto
+	valor := mensaje.Valor
+	tienda := mensaje.Tienda
+	destino := mensaje.Destino
+	prioritario := mensaje.Prioritario
 
 	log.Printf("Nueva Orden Generada con id de producto: %s", id)
 	
 	filasRegistro := leerFilasRegistro("registroLogistica")
 	var nuevaFila[]string
+	strSeguimiento := strconv.Itoa(IteradorIdSeguimiento)
 	nuevaFila = append(nuevaFila, id, producto, valor, tienda, destino, prioritario, strSeguimiento)
 	filasRegistro = append(filasRegistro, nuevaFila)
 	escribirFilasRegistro("registroLogistica", filasRegistro)
-	strSeguimiento := strconv.Itoa(IteradorIdSeguimiento)
-	return &IdSeguimiento{id: strSeguimiento}, nil
+	return &IdSeguimiento{Id: strSeguimiento}, nil
 }
 
 func (s *Server) ConsultarOrden(ctx context.Context, mensaje *IdSeguimiento) (*MensajeReply, error) {
-	log.Printf("Consulta desde Cliente a id de seguimiento: %s", mensaje.id)
+	log.Printf("Consulta desde Cliente a id de seguimiento: %s", mensaje.Id)
 	return &MensajeReply{Respuesta1: "Hola Cliente qlo"}, nil
 }
 
