5828cd08a59f1088ce8692bdb17d2ef6428aec76
iramirezu <ignacio@lemonpot.com>
2020-10-19 15:06:53 -0300

    pa

--- Logistica/server.go
+++ Logistica/server.go
@@ -13,7 +13,7 @@ import (
 
 	"google.golang.org/grpc"
 	"github.com/PrestigioExpress/ServicioCliente/chatCliente"
-	"github.com/PrestigioExpress/ServicioCamion/chatCamion"
+	"github.com/PrestigioExpress/ServicioCliente/chatCamion"
 )
 
 
