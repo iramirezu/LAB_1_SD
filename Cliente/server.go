91b2034751f87481d779be5bcb47a01f7a5e76b2
iramirezu <ignacio@lemonpot.com>
2020-10-19 03:46:52 -0300

    pa

--- Cliente/server.go
+++ Cliente/server.go
@@ -21,11 +21,11 @@ func main()  {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := chatCamion.Server{}
+	s := chatCliente.Server{}
 
 	grpcServer := grpc.NewServer()
 
-	chatCamion.RegisterServicioClienteServer(grpcServer, &s) // actualizar
+	chatCliente.RegisterServicioClienteServer(grpcServer, &s) // actualizar
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
