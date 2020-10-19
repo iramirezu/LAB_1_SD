7dae9c11876a133e8e1ec38fc6348957eeb9045d
iramirezu <ignacio@lemonpot.com>
2020-10-19 12:48:12 -0300

    pa

--- Cliente/chatCliente/chatCliente.go
+++ Cliente/chatCliente/chatCliente.go
@@ -3,7 +3,7 @@ package chatCliente
 import (
 	"log"
 	"encoding/csv"
-	//"strconv"
+	"strconv"
 	"os"
 	"golang.org/x/net/context"
 
@@ -44,7 +44,7 @@ func (s *Server) ConsultarOrden(ctx context.Context, mensaje *IdSeguimiento) (*M
 
 func cantFilasRegistro(nombreRegistro string) [][]string { // numero de seguimiento sera un autogenerado que se relaciona directamente con la cantidad de registros
     rows := leerFilasRegistro(nombreRegistro)
-	cant := (rows.leng - 1)	
+	cant := (len(rows) - 1)	
     return cant
 }
 
